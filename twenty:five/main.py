655097cdb7b08609efc8fb42d0ef4a987bbfce50
bitsyzed <114426905+bitsyzed@users.noreply.github.com>
2025-10-23 12:14:20 -0400

    Refactor turn management and scoring logic
    sure copilot i guess 
    
    Signed-off-by: bitsyzed <114426905+bitsyzed@users.noreply.github.com>

--- twenty:five/main.py
+++ twenty:five/main.py
@@ -5,58 +5,70 @@ import random
 # Global Variables
 player_score = 0
 cpu_score = 0
-turn = 1
+turn = True
+player_state = "safe"
+stand_streak = 0
 
 # 1 is player, 2 is cpu
 
 # Actions
 def take_action(action):
-    global turn, cpu_score, player_score
+    global turn, cpu_score, player_score, stand_streak
 
+    if turn == True:
+        display.show("P")
+    elif turn == False:
+        display.show("C")
+    sleep(1500)
+    display.clear()
+    
     if action == "hit":
-        if turn == 1:
-            player_score = player_score + random.randint(1, 11)
+        stand_streak = 0
+        if turn == True:
+            player_score += random.randint(2, 11)
+            update_screen()
             print("Turn 1: Hit")
-            turn = 2
-        elif turn == 2:
-            cpu_score = cpu_score + random.randint(1, 11)
+            turn = False
+        elif turn == False:
+            cpu_score += random.randint(2, 11)
+            update_screen()
             print("Turn 2: Hit")
-            turn = 1
-
-    if action == "stand":
-        if turn == 1:
-            print("Turn 1: Stand")
-            turn = 2
-        elif turn == 2:
-            print("Turn 2: Stand")
-            turn = 1
+            turn = True
+        sleep(1000)
         
-
+    if action == "stand":
+        stand_streak += 1
+        if turn == True:
+            print("Turn 1: Stand")
+            turn = False
+        elif turn == False:
+            print("Turn 2: Stand")
+            turn = True
+        
 def turn_manager():
     global turn, cpu_score
     
     # Player actions.
-    if turn == 1:
+    if turn == True:
         if button_a.was_pressed():
             take_action("hit")
         if button_b.was_pressed():
             take_action("stand")
-
-    # CPU actions.
-    if turn == 2:
+    elif turn == False: #CPU actions.
         sleep(1000)
-        if cpu_score <= 16:
+        if cpu_score <= 20:
             take_action("hit")
-        if cpu_score >= 17:
+        if cpu_score >= 21:
             take_action("stand")
-
 
 def update_screen():
     global turn, player_score
 
-    for i in range(player_score):
-        pass
-        
+    if player_score > 25:
+        display.show(Image.SAD)
+    else:
+        for i in range(player_score):
+            display.set_pixel(i%5,i//5,9)
+
 while True:
     turn_manager()
-    update_screen()
